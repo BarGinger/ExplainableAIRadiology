3d7747373842be5800bd2c5064896fdf25dd9aa9
Bar Melinarskiy <b.melinarskiy@students.uu.nl>
2025-02-17 13:44:34 +0100

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
 import pandas as pd
+import os
 
 
 class CheXpertDataset(Dataset):
@@ -18,7 +19,14 @@ class CheXpertDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.dataframe.iloc[idx]['path']
-        #         img_path = "/content/drive/MyDrive/" + img_path
+
+        if os.path.exists('/chexpert_data'):
+          FOLDER = '/chexpert_data/'
+        elif os.path.exists('/content/drive/MyDrive'):
+          FOLDER = '/content/drive/MyDrive/CheXpert-v1.0-small/'
+        else:
+          FOLDER = "/CheXpert-v1.0-small/" # Or handle the case where the directory doesn't exist
+        img_path = FOLDER + img_path
         image = Image.open(img_path).convert('RGB')  #rgb format
         labels = self.dataframe.iloc[idx][self.class_names].values.astype('float32')  # astype float32 otherwise error
         labels = torch.tensor(labels, dtype=torch.float32)
